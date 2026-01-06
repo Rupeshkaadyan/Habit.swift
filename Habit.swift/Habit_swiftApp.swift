5cd9f90bafd4d2a9d5d5c5450939c68ce0f29da1
Rupeshkaadyan <rupeshjat5@gmail.com>
2026-01-07 01:21:41 +0530

    Initial Commit

---
+++ Habit.swift/Habit_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_swiftApp.swift
+//  Habit.swift
+//
+//  Created by Ethan on 07/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Habit_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
